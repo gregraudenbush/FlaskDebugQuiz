80cf8aac21ee841b9a299c8bc10137343e40f6a9
thinhle93 <thinhle93@gmail.com>
2018-02-12 15:14:26 -0800

    Thinh's modification

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, session, flash
+from flask import Flask, request, render_template, redirect, session, flash
 app = Flask(__name__)
 app.secret_key = "unicorns"
 
@@ -16,19 +16,26 @@ def index():
     if "info" not in session:
         session['info'] = ""
     else:
+
         for x in session['info']:
+           
             if x == "pizza":
-                x = "pumkin pie"
+                session['info'].pop()
+                session['info'].append("pumkin pie")
+                # x = "pumkin pie"
+       
+
+
     ##########################
     #Important!
     #Fix the above code.....
     # Session['info'] should display "pumkin pie" and NOT "pizza"
     #########################
     
-    return render_template("index.html", info = session['info'])
+    return render_template("/index.html", info = session['info'])
 
 
-@app.route("/form")
+@app.route("/form", methods=['POST'])
 def form():
     
     if len(request.form['FirstName']) < 1 or len(request.form['Last_Name']) < 1:
