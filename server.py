7d4e8649ef5e9d56d6fb6a90956b2f8993e98861
LawerenceLee <lucifitz.edward@gmail.com>
2018-03-12 15:15:32 -0700

    fixed 4 errors

--- server.py
+++ server.py
@@ -1,44 +1,27 @@
-from flask import Flask, request, redirect, session, flash
+from flask import Flask, request, redirect, session, flash, render_template
+
+
 app = Flask(__name__)
 app.secret_key = "unicorns"
 
-####################
-#Welcome to the Flask Debug Quiz
-#Fix all errors, and fix the commented instructions within the index route
-#Good Luck Hackers!
-####################
 
-@app.route('/') 
-def index(): 
-    
-
-
+@app.route('/')
+def index():
     if "info" not in session:
         session['info'] = ""
-    else:
-        for x in session['info']:
-            if x == "pizza":
-                x = "pumkin pie"
-    ##########################
-    #Important!
-    #Fix the above code.....
-    # Session['info'] should display "pumkin pie" and NOT "pizza"
-    #########################
-    
-    return render_template("index.html", info = session['info'])
+    return render_template("index.html")
 
 
-@app.route("/form")
+@app.route("/form", methods=["GET", "POST"])
 def form():
-    
-    if len(request.form['FirstName']) < 1 or len(request.form['Last_Name']) < 1:
+    if len(request.form['FirstName']) < 1 or len(request.form['LastName']) < 1:
         flash("Please Complete Form")
     else:
-        session['info'] = [request.form["FirstName"], request.form['Last_Name'], request.form['FaveSnack']]
-
+        session['info'] = [request.form["FirstName"], request.form['LastName'],
+                           request.form['FaveSnack']]
 
     return redirect('/')
 
 
-
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
